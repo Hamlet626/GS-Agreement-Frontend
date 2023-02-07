805857b1af830eb1d83bd2f800c853585cdcdb5a
Jader Moura <jader_moura@epam.com>
2023-02-07 21:13:00 +0100

    feat: material ui configured

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material";
+import { Container } from "@mui/system";
+
+export default function Header() {
+  return (
+    <div
+      style={{
+        background: "linear-gradient(135deg, #dea27a, #fff7f2)",
+        height: "300px",
+      }}
+    >
+      <Container>Header</Container>
+    </div>
+  );
+}
---
+++ config/theme.tsx
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f15",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+});
+export default theme;
