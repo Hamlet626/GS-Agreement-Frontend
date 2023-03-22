2bcc1e2c4fd4466542e0643440db435c41658264
Jader Moura <jader.moura.gg@gmail.com>
2023-03-22 20:47:27 +0100

    header highlight

--- components/Header/styles.ts
+++ components/Header/styles.ts
@@ -1,5 +1,6 @@
 import { styled } from "@stitches/react";
-
+import { Button as MaterialButton } from "@mui/material";
+import { theme } from "../../config/theme";
 
 export const Navbar = styled("div", {
   flexGrow: 1,
@@ -8,3 +9,16 @@ export const Navbar = styled("div", {
   justifyContent: "flex-end",
 });
 
+export const Button = styled(MaterialButton, {
+  borderRadius: 0,
+  borderBottom: "3px solid transparent",
+  variants: {
+    actived: {
+      true: {
+        borderBottom: `3px solid ${theme.colors.$primaryLight}`,
+        fontWeight: "bolder",
+        color: theme.colors.$primaryLight,
+      },
+    },
+  },
+});
