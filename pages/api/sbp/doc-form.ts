335a4d1fac8be6b3e1287c6ec43b0ce1a98145d0
Jader Moura <jader.moura.gg@gmail.com>
2023-03-24 23:58:37 +0100

    fix: error 3 test

--- pages/api/sbp/doc-form.ts
+++ pages/api/sbp/doc-form.ts
@@ -5,6 +5,557 @@ import openAiChat from "../../../utils/openAiChat";
 // import { constructPrompt } from "openai_embedding";
 import {unmergeDates} from "../../../utils/datesFolder";
 
+const fake1 = {
+  "certain_payments": {
+    "July 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.00
+      }
+    ],
+    "August 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.00
+      }
+    ],
+    "September 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.00
+      },
+      {
+        "type": "Monthly non-accountable allowance",
+        "amount": 300.00
+      },
+      {
+        "type": "Multiple Pregnancy Compensation",
+        "amount": 7000.00
+      }
+    ],
+    "October 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.00
+      },
+      {
+        "type": "Monthly non-accountable allowance",
+        "amount": 300.00
+      }
+    ],
+    "November 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.00
+      },
+      {
+        "type": "Monthly non-accountable allowance",
+        "amount": 300.00
+      }
+    ],
+    "December 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.00
+      },
+      {
+        "type": "Monthly non-accountable allowance",
+        "amount": 300.00
+      }
+    ],
+    "January 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.00
+      },
+      {
+        "type": "Monthly non-accountable allowance",
+        "amount": 300.00
+      }
+    ],
+    "February 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.00
+      },
+      {
+        "type": "Monthly non-accountable allowance",
+        "amount": 300.00
+      }
+    ],
+    "March 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.00
+      },
+      {
+        "type": "Monthly non-accountable allowance",
+        "amount": 300.00
+      },
+      {
+        "type": "Injection Medication Start Fee",
+        "amount": 600.00
+      }
+    ],
+    "April 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.00
+      },
+      {
+        "type": "Monthly non-accountable allowance",
+        "amount": 300.00
+      }
+    ],
+    "May 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.00
+      },
+      {
+        "type": "Monthly non-accountable allowance",
+        "amount": 300.00
+      }
+    ],
+    "June 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.00
+      },
+      {
+        "type": "Monthly non-accountable allowance",
+        "amount": 300.00
+      },
+      {
+        "type": "Maternity clothing allowance",
+        "amount": 800.00
+      }
+    ]
+  },
+  "uncertain_payments": [
+    {
+      "type": "Embryo Transfer Fee",
+      "amount": 1000.00
+    },
+    {
+      "type": "Travel Reimbursement",
+      "amount": "TBD"
+    },
+    {
+      "type": "Evaluation/Cancelled Cycle",
+      "amount": 500.00
+    },
+    {
+      "type": "Invasive Procedure",
+      "amount": "500.00 to 7000.00"
+    },
+    {
+      "type": "Bed Rest/Activity Restriction",
+      "amount": "250.00/Week"
+    },
+    {
+      "type": "Breast Milk",
+      "amount": "300.00/Week"
+    },
+    {
+      "type": "Life Insurance",
+      "amount": "TBD"
+    },
+    {
+      "type": "Health Insurance",
+      "amount": "TBD"
+    },
+    {
+      "type": "Lost Wages (Net Hourly Rate)",
+      "amount": "TBD"
+    }
+  ]
+}
+const fake2 = {
+  "certain_payments": {
+    "Aug 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.0
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300.0
+      }
+    ],
+    "Sep 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.0
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300.0
+      }
+    ],
+    "Oct 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.0
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300.0
+      }
+    ],
+    "Nov 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.0
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300.0
+      }
+    ],
+    "Dec 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.0
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300.0
+      }
+    ],
+    "Jan 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.0
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300.0
+      }
+    ],
+    "Feb 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.0
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300.0
+      }
+    ],
+    "Mar 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.0
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300.0
+      },
+      {
+        "date": 10,
+        "type": "Injection Medication Start Fee",
+        "amount": 600.0
+      }
+    ],
+    "Apr 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.0
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300.0
+      },
+      {
+        "date": 20,
+        "type": "Multiple Pregnancy Compensation",
+        "amount": 7000.0
+      },
+      {
+        "date": 20,
+        "type": "Embryo Transfer Fee",
+        "amount": 1000.0
+      }
+    ],
+    "May 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.0
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300.0
+      },
+      {
+        "date": 31,
+        "type": "Maternity Clothing Allowance",
+        "amount": 800.0
+      }
+    ],
+    "Jun 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.0
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300.0
+      }
+    ],
+    "Jul 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180.0
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300.0
+      },
+      {
+        "date": 19,
+        "type": "Multiple Pregnancy Compensation",
+        "amount": 7000.0
+      }
+    ]
+  },
+  "uncertain_payments": [
+    {
+      "type": "Breast Milk",
+      "amount": 300.0
+    }
+  ]
+}
+const fake3 = {
+  "certain_payments": {
+    "Sep 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180
+      },
+      {
+        "date": 1,
+        "type": "Multiple Pregnancy Compensation",
+        "amount": 7000
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300
+      },
+      {
+        "date": 1,
+        "type": "Contract Signing Bonus",
+        "amount": 500
+      },
+      {
+        "date": 1,
+        "type": "Matching Bonus",
+        "amount": 500
+      }
+    ],
+    "Oct 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300
+      }
+    ],
+    "Nov 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300
+      }
+    ],
+    "Dec 2023": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300
+      }
+    ],
+    "Jan 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300
+      }
+    ],
+    "Feb 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300
+      }
+    ],
+    "Mar 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300
+      },
+      {
+        "date": 30,
+        "type": "Injection Medication Start Fee",
+        "amount": 600
+      }
+    ],
+    "Apr 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300
+      },
+      {
+        "date": 20,
+        "type": "Embryo Transfer Fee",
+        "amount": 1000
+      }
+    ],
+    "May 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300
+      },
+      {
+        "date": 31,
+        "type": "Maternity Clothing Allowance",
+        "amount": 800
+      }
+    ],
+    "Jun 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300
+      }
+    ],
+    "Jul 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300
+      },
+      {
+        "date": 19,
+        "type": "Multiple Pregnancy Compensation",
+        "amount": 7000
+      }
+    ],
+    "Aug 2024": [
+      {
+        "date": 1,
+        "type": "Main Pregnancy Compensation",
+        "amount": 4180
+      },
+      {
+        "date": 1,
+        "type": "Monthly Non-Accountable Allowance",
+        "amount": 300
+      }
+    ]
+  },
+  "uncertain_payments": [
+    {
+      "type": "Lost Wages",
+      "amount": "TBD"
+    },
+    {
+      "type": "Breast Milk",
+      "amount": 300
+    }
+  ]
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!openaiConfig.apiKey) {
     res.status(500).json({
@@ -47,7 +598,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     console.log(sbpLastChoice?.content);
     res.status(200).json({
-      sbpPaymentTabs: sbpLastChoice?.content.replace(/TBD/g, '0'),
+      // sbpPaymentTabs: sbpLastChoice?.content.replace(/TBD/g, '0'),
+      sbpPaymentTabs: JSON.stringify(fake2).replace(/TBD/g, '0'),
     });
   } catch (error: any) {
     res.status(500).end({
